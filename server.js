539ac42d575be0e8142bee6a3cd6b4d4b4dc9583
spwai <92832744+spwai@users.noreply.github.com>
2023-12-16 09:03:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
-const app = express();
-const PORT = 3000;
-
 app.use((req, res, next) => {
   const ip = req.ip || req.connection.remoteAddress;
   console.log(`Incoming request from IP: ${ip}`);
 
   // Specify the full path of the file on your desktop
-  const filePath = path.join('C:\Users\asabe\desktop', 'ip_log.txt');
+  const filePath = path.join('C:\Users\asabe\Desktop\spwai', 'ip_log.txt');
+
+  // Log to console
+  console.log(`Attempting to log IP to ${filePath}`);
+
+  // Log to file
   fs.appendFile(filePath, `${ip}\n`, (err) => {
-    if (err) throw err;
-    console.log(`IP logged to ${filePath}`);
+    if (err) {
+      console.error(`Error logging IP: ${err.message}`);
+    } else {
+      console.log(`IP logged to ${filePath}`);
+    }
   });
 
   next();
-});
-
-app.get('/', (req, res) => {
-  res.send('Ready');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
 });
