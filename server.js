8a3e3be90f65f451e36207df97d2a4526aa324fe
Nitro <spwai03@gmail.com>
2023-12-16 09:25:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,18 +11,18 @@ console.log('Express and other dependencies loaded...');
 
 // Your middleware
 app.use((req, res, next) => {
-  console.log('Middleware is being executed...');
+  console.log('Inside the middleware...');
 
   const ip = req.ip || req.connection.remoteAddress;
   console.log(`Incoming request from IP: ${ip}`);
 
-  // Specify the full path of the file on your desktop
-  const filePath = path.join('C:\\Users\\asabe\\Desktop\\spwai', 'ip_log.txt');
+  // Specify a relative path for the log file (in the same directory as server.js)
+  const filePath = path.join(__dirname, 'ip_log.txt');
 
   // Log to console
   console.log(`Attempting to log IP to ${filePath}`);
 
-  // Log to file
+  // Log to file with error handling
   fs.appendFile(filePath, `${ip}\n`, (err) => {
     if (err) {
       console.error(`Error logging IP: ${err.message}`);
