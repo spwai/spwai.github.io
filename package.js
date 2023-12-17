69e5e0e042da9ecdd40f61a50dec32eb820dd193
spwai <92832744+spwai@users.noreply.github.com>
2023-12-16 23:11:04 -0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,28 @@
+{
+  "name": "Ip Logger",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1"
+    // Add other dependencies as needed
+  },
+  "engines": {
+    "node": ">=12.0.0"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/spwai/spwai.github.io.git"
+  },
+  "keywords": [
+    "node",
+    "heroku",
+    "express"
+    // Add other keywords as needed
+  ],
+  "author": "Spwai",
+  "license": "MIT"
+}
